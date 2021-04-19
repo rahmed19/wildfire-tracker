e3d7067ac4774b500c69130d7250bd46e8e4e686
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-19 10:42:29 -0700

    Location Marker CSS

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -2,9 +2,10 @@ import React from 'react'
 import {Icon} from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/fire-alert'
 
-export default function LocationMarker() {
+export default function LocationMarker({ lat, lng, onClick }) {
     return (
-        <div>
+        <div className="location-marker" onClick={onClick}>
+            <Icon icon={locationIcon} className="location-icon" />
             
         </div>
     )
--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
 
 
 export default function Maps ({center, zoom}) {
@@ -10,6 +11,7 @@ export default function Maps ({center, zoom}) {
                 defaultCenter={center}
                 defaultZoom={zoom}
             >
+                <LocationMarker lat={center.lat} lng={center.lng} />
             </GoogleMapReact>
         </div>
     )
