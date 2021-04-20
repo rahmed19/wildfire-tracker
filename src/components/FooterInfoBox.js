47c8f41b7b6d43f634e9d88db67faefeabb42867
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-20 15:56:59 -0700

    Modal CSS update

--- src/components/FooterInfoBox.js
+++ src/components/FooterInfoBox.js
@@ -3,8 +3,19 @@ import React, { useState } from 'react'
 export default function FooterInfoBox({ showInfo, setShowInfo }) {
     console.log("hello from footer info")
     return (
-        <div className="footer-info" onClick={() => setShowInfo(false)}>
-            <h2>Footer Info</h2>
+        <div className="modal-wrapper" onClick={() => setShowInfo(false)}>
+            <div className="modal-backdrop" />
+            <div className="modal-box">
+                <div className="modal-content">
+                    <h2>Project Information</h2>
+                    <p>
+                        This demo ReactJS project was built largely based
+                        on <a href="https://www.youtube.com/watch?v=ontX4zfVqK8" target="new">
+                            this YouTube tutorial. My additions and changes include:
+                </a>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
