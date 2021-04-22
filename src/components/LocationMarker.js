824d560fe3fb9ece70471f7afe0226c4b2a47e65
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-22 06:54:41 -0700

    onclick function test

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -6,10 +6,10 @@ import locationIconVolcano from '@iconify/icons-mdi/mountain'
 export default function LocationMarker({ lat, lng, onClick, eventCategory }) {
 
     return (
-        <div className="location-marker" onClick={onClick}>
+        <div className="location-marker" onKeyPress={() => onClick()} onClick={() => onClick()} >
             {eventCategory === "wildfires" ?
-                <Icon icon={locationIconWildfire} className="location-icon" /> :
-                <Icon icon={locationIconVolcano} className="location-icon" />}
+                <Icon onMouseDown={() => onClick()} icon={locationIconWildfire} className="location-icon" /> :
+                <Icon onClick={() => onClick()} icon={locationIconVolcano} className="location-icon" />}
         </div>
     )
 }
