07e5c08e98ea785d2aeb327ba42d220ecd5257bb
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-23 22:00:43 -0700

    Modal readjustment

--- src/components/FooterInfoBox.js
+++ src/components/FooterInfoBox.js
@@ -2,9 +2,22 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 export default function FooterInfoBox({ showInfo, setShowInfo }) {
+
     return (
         <>
-
+            <div className="modal-container">
+                <div className="modal">
+                    <div className="modal-column">
+                        Column 1
+                    </div>
+                    <div className="modal-column">
+                        Column 2
+                    </div>
+                    <div className="modal-column">
+                        Column 3
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
