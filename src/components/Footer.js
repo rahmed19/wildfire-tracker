a9bc01d48fd4008a67c1ff505d95b97c2cb735dc
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-24 06:36:24 -0700

    import styled components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 import FooterInfoBox from './FooterInfoBox'
 
 export default function Footer() {
-    const [showInfo, setshowInfo] = useState(false)
+    const [showInfo, setShowInfo] = useState(false)
 
     return (
         <footer className="footer">
             <div className="footer-flex-container">
                 <div className="footer-column">
-                    <h3 onClick={() => setshowInfo((prevState) => !prevState)}
+                    <h3 onClick={() => setShowInfo((prevState) => !prevState)}
                         className="footer-h3">Learn more about this demo ReactJS project.</h3>
-                    {showInfo && <FooterInfoBox showInfo={showInfo} setShowInfo={setshowInfo} />}
+                    {showInfo && <FooterInfoBox showInfo={showInfo} setShowInfo={setShowInfo} />}
                 </div>
             </div>
         </footer >
--- src/components/FooterInfoBox.js
+++ src/components/FooterInfoBox.js
@@ -1,12 +1,102 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import modal from '../images/modal4.jpg'
+import styled from 'styled-components'
+import { MdClose } from 'react-icons/md'
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    z-index: -100;
+
+`
+const Background = styled.div`
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: -100;
+`
+const ModalWrapper = styled.div`
+    width: 800px;
+    height: 500px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+    background: white;
+    color: black;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    position: relative;
+    z-index: -100;
+    border-radius: 10px;
+    z-index: -100;
+
+`
+const ModalImg = styled.img`
+    width: 100%;
+    height: 100%
+    border-radius: 10px 0 0 10px;
+    background: black;
+    z-index: -100;
+
+`
+const ModalContent = styled.div`
+    display: flex;
+    justify-conten: center;
+    align-items: center;
+    line-height: 1.8;
+    color: #141414;
+    z-index: -100;
+
+    p {
+        margin-bottom: 1rem;
+
+    }
+`
+
+const CloseModal = styled(MdClose)`
+    cursor: pointer;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 32px;
+    height: 32px;
+    padding: 0;
+    z-index: -100;
+`
+
 
 export default function FooterInfoBox({ showInfo, setShowInfo }) {
 
     return (
         <>
-            <div className="modal-container">
+            <Container>
+                <Background>
+                    <ModalWrapper>
+                        <ModalImg src={modal} alt="camera" />
+                        <ModalContent>
+                            <h1>Are you ready?
+                            <p>Some stuff goes here.</p>
+                            </h1>
+                        </ModalContent>
+                        <CloseModal aria-label="Close Modal" onClick={() => setShowInfo((prevState) => !prevState)} />
+                    </ModalWrapper>
+                </Background>
+            </Container>
+
+            {/* <div className="modal-container">
                 <div className="modal">
                     <div className="modal-column">
                         <img src={modal} />
@@ -39,7 +129,7 @@ export default function FooterInfoBox({ showInfo, setShowInfo }) {
 
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
